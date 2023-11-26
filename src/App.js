d3360f41bc72d38592546800401583619e4d97b6
bundasse <ymtotolove@gmail.com>
2023-11-26 15:31:33 +0900

    1126 todo-list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const OnChangeInput = (event) => setTodo(event.target.value)
+  const OnSubmitForm = (event)=> {
+    event.preventDefault();
+    if(todo === ""){
+      return;
+    } 
+    setTodos((current)=> [todo, ...current])
+    setTodo("");
+  }
+  console.log(todos);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MY TO-DO LIST ({todos.length})</h1>
+      <form onSubmit={OnSubmitForm}>
+        <input type='text' value={todo} placeholder='Write your to do!' onChange={OnChangeInput}></input>
+        <button>Add Todo</button>
+      </form>
     </div>
   );
 }
